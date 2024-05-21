d4bef72f011360bc4d6636f5585ef4169cb520fe
Maryam Nasir <maryamnasir555@yahoo.com>
2024-05-21 16:58:20 +0500

    Add fetch threads action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -12,6 +12,40 @@ interface ThreadParams {
   path: string;
 }
 
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts that have no parents (top-level threads...)
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to get threads: ${error.message}`);
+  }
+}
+
 export async function createThread({
   text,
   author,
