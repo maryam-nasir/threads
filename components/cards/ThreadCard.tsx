72b4348b37f7b30a40b57729852468ddbda31ecc
Maryam Nasir <maryamnasir555@yahoo.com>
2024-05-31 10:17:44 +0500

    Add different style for comment container

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,13 +37,18 @@ const ThreadCard = ({
   isComment,
 }: ThreadCardProps) => {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
             <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
               <Image
-                src={author.image}
+                // src={author.image}
+                src={"/assets/heart-gray.svg"}
                 alt="Profile image of author"
                 fill
                 className="cursor-pointer rounded-full"
