8c2e863052fc489d25af76b16379170e0afc4308
Maryam Nasir <maryamnasir555@yahoo.com>
2024-06-07 18:04:27 +0500

    Fix onboarding user info object

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,17 +1,24 @@
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs/server";
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null;
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+
+  if (userInfo?.onboarded) {
+    redirect("/");
+  }
 
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
+    username: userInfo ? userInfo?.username : user?.username,
+    name: userInfo ? userInfo?.name : user?.firstName || "",
+    bio: userInfo ? userInfo?.bio : "",
     image: userInfo?.image || user?.imageUrl,
   };
 
